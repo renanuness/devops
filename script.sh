b7aa4cd89cffc7629883560c24a8f0d45080fa21
root <root@controle>
2025-06-14 13:45:16 +0300

    remove all

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get update
-cd /tmp
-wget https://apt.puppet.com/puppet7-release-bullseye.deb
-dpkg -i puppet7-release-bullseye.deb
-#apt-get upgrade -y
-apt-get update 
-apt-get install vim puppet-agent -y
